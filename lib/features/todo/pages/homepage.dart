9e92506f35e784aa95cf2bea6394e21c016889ca
palminschi <mihaipalminschi@gmail.com>
2023-11-27 11:17:31 +0100

    HomePage AppBar

--- lib/features/todo/pages/homepage.dart
+++ lib/features/todo/pages/homepage.dart
@@ -1,19 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tasks_management/common/utils/constants.dart';
 import 'package:tasks_management/common/widgets/appstyle.dart';
+import 'package:tasks_management/common/widgets/custom_text_field.dart';
 import 'package:tasks_management/common/widgets/reusable_text.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends ConsumerStatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  ConsumerState<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends ConsumerState<HomePage> {
+  final TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false,
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(85),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.w),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ReusableText(
+                        text: "Dashboard",
+                        style: appStyle(18, AppConst.kLight, FontWeight.bold)),
+                    Container(
+                      width: 30,
+                      height: 30,
+                      decoration: const BoxDecoration(
+                          color: AppConst.kLight,
+                          borderRadius: BorderRadius.all(Radius.circular(9))),
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: const Icon(
+                          Icons.add,
+                          color: AppConst.kBkDark,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              CustomTextField(
+                hintText: "Search",
+                controller: searchController,
+                prefixIcon: Container(
+                  padding: const EdgeInsets.all(4),
+                  child: GestureDetector(
+                    onTap: null,
+                    child: const Icon(
+                      AntDesign.search1,
+                      color: AppConst.kGreyLight,
+                    ),
+                  ),
+                ),
+                suffixIcon: const Icon(
+                  FontAwesome.sliders,
+                  color: AppConst.kGreyLight
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
       body: Column(children: [
         ReusableText(
             text: "Hello",
--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
             useMaterial3: true,
           ),
           themeMode: ThemeMode.dark,
-          home: const OnBoardingPage(),
+          home: const HomePage(),
         );
       }
     );
